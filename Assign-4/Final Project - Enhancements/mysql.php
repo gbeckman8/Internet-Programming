b0b217ae90fa0ab194a6c8d323bad3b171065265
gbeckman8 <gbeckman@students.kennesaw.edu>
2019-11-15 10:31:41 -0500

    First Commit for final project

---
+++ Assign-4/Final Project - Enhancements/mysql.php
@@ -0,0 +1,8 @@
+<?php
+//skhpa2@yahoo.com
+$resid=MySQLi_Connect('localhost','gbeckman','gray','gbeckman');
+					if(MySQLi_Connect_Errno()) {
+						echo "<tr align='center'> <td colspan='5'> Failed to connect to MySQL </td> </tr>";
+					}
+					else {
+					}
