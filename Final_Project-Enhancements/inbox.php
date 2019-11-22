453c4e4e7531805a5e34990e5dd8d434df3c965d
gbeckman8 <gbeckman@students.kennesaw.edu>
2019-11-22 12:23:52 -0500

    inbox

--- Final_Project-Enhancements/inbox.php
+++ Final_Project-Enhancements/inbox.php
@@ -2,84 +2,77 @@
 <html>
 
 <head>
-	<link rel='stylesheet' href='page_css.css'>
+	<link rel='stylesheet' href='Grayson_css.css'>
 	<title> Student's Hangout </title>
 </head>
 
-<body>
-	<table cellpadding='3' cellspacing='3' class='tab_main'>
+<div class="container">
+	<body>
 		<!--Logo-->
-		<tr>
-			<td colspan='5'><img src='images/logo.png' height='65%' width='100%'></td>
-			<!--1350x160-->
-		</tr>
+		<img src='images/logo.png' height='65%' width='100%'>
 		<!--Nav_Tabs-->
-		<tr align='center' bgcolor='lightgrey' class='td_bor'>
-			<td width='5%'> <?php Session_start();
-							if (isset($_SESSION["user_id"])) {
-								echo "<a href='user_page.php'>";
-							} else {
-								echo "<a href='home.php'>";
-							} ?>Home </a></td>
-			<td width='5%'> <a href='send_message.php'>Send Message </a></td>
-			<td width='5%'> <a href='inbox.php'>Inbox (Only Recent Message)</a></td>
-			<td width='5%'> <a href='view_profile.php'>View Profile </a></td>
-			<td width='5%'> <a href='signout.php'>Signout </a></td>
+		<table width='100%'>
+			<tr>
+				<td valign='top'>
+					<div class="vertical-menu-long">
+						<?php Session_start();
+						if (isset($_SESSION["user_id"])) {
+							echo "<a href='user_page.php'>";
+						} else {
+							echo "<a href='home.php'>";
+						} ?>Home </a>
+						<a href='send_message.php'>Send Message </a>
+						<a href='inbox.php'>Inbox (Only Recent Message) </a>
+						<a href='view_profile.php'>View Profile </a>
+						<a href='signout.php'>Signout </a>
+					</div>
+				</td>
 
-		</tr>
+				<td> </td>
+				<td valign='top', align='right'>
+					<div class="vertical-menu">
+						<a href='update_status.php'> Status Update </a>
+						<a href='friends.php'>Friends </a>
+						<a href='friend_list.php'>Friend List</a>
+					</div>
+				</td>
+			</tr>
 
-		<tr>
-			<td>
-				<hr>
-			</td>
-			<td>
-				<hr>
-			</td>
-			<td>
-				<hr>
-			</td>
-			<td>
-				<hr>
-			</td>
-			<td>
-				<hr>
-			</td>
-		</tr>
+			<?php
+			include 'mysql.php';
 
-		<?php
-		include 'mysql.php';
+			//Session_start();
+			if (isset($_SESSION["user_id"])) {
+				$id = $_SESSION["user_id"];
+				$query = "select * from messages where receiver_id=" . $id . " order by id desc";
+				if ($resid) {
+					$result = MySQLi_Query($resid, $query);
+					$data = MySQLi_Fetch_Row($result);
+					if ($data) {
+						$query = "select name,email from students where id=" . $data[1] . "";
+						$sender = MySQLi_Query($resid, $query);
+						$sender = MySQLi_Fetch_Row($sender);
+						//if($data) {
 
-		//Session_start();
-		if (isset($_SESSION["user_id"])) {
-			$id = $_SESSION["user_id"];
-			$query = "select * from messages where receiver_id=" . $id . " order by id desc";
-			if ($resid) {
-				$result = MySQLi_Query($resid, $query);
-				$data = MySQLi_Fetch_Row($result);
-				if ($data) {
-					$query = "select name,email from students where id=" . $data[1] . "";
-					$sender = MySQLi_Query($resid, $query);
-					$sender = MySQLi_Fetch_Row($sender);
-					//if($data) {
-
-					echo "<tr align='center'> <td colspan='5'> <table cellpadding='4' cellspacing='5' width='100%' style='table-layout:fixed'> <col width='100%'> ";
-					echo "<td>From:- <font color='blue'>" . $sender[0] . " </font> [" . $sender[1] . "] </td> </tr>";
-					echo "<tr> <td style='word-wrap:break-word'>Message:-" . $data[3] . "</td> </tr>";
-					echo "</table> </td> </tr>";
-				} else {
-					echo "<tr align='center'> <td colspan='5'> <font color='lightblue'> No Messages! </font> </td> </tr>";
+						echo "<tr align='center'> <td colspan='5'> <table cellpadding='4' cellspacing='5' width='100%' style='table-layout:fixed'> <col width='100%'> ";
+						echo "<td>From:- <font color='blue'>" . $sender[0] . " </font> [" . $sender[1] . "] </td> </tr>";
+						echo "<tr> <td style='word-wrap:break-word'>Message:-" . $data[3] . "</td> </tr>";
+						echo "</table> </td> </tr>";
+					} else {
+						echo "<tr align='center'> <td colspan='5'> <font color='black'> No Messages! </font> </td> </tr>";
+					}
+					MySQLi_Close($resid);
 				}
-				MySQLi_Close($resid);
+			} else {
+				echo "<tr align='center'> <td colspan='5'> <font color='red'> Sorry, You not Logged in! </font> Login again:- <a href='login.php'>Login</a> </td> </tr>";
 			}
-		} else {
-			echo "<tr align='center'> <td colspan='5'> <font color='red'> Sorry, You not Logged in! </font> Login again:- <a href='login.php'>Login</a> </td> </tr>";
-		}
 
-		?>
-	</table>
-	<footer align='center'>
-		&copy; All rights Reserved https://github.com/abhn/simple-php-mysql-project.
-	</footer>
-</body>
+			?>
+		</table>
+		<footer align='center'>
+			&copy; All rights Reserved https://github.com/abhn/simple-php-mysql-project.
+		</footer>
+	</body>
+</div>
 
 </html>
