810da55e4b91eba16ea6e33ff30e613b257783c8
gbeckman8 <gbeckman@students.kennesaw.edu>
2019-11-25 10:38:07 -0500

    CSS complete.

--- Final_Project-Enhancements/view_profile.php
+++ Final_Project-Enhancements/view_profile.php
@@ -2,54 +2,38 @@
 <html>
 
 <head>
-	<link rel='stylesheet' href='page_css.css'>
+	<link rel='stylesheet' href='Grayson_css.css'>
 	<title> Student's Hangout </title>
 </head>
 
-<body>
-	<table cellpadding='3' cellspacing='3' class='tab_main'>
+<div class="container">
+
+	<body>
 		<!--Logo-->
-		<tr>
-			<td colspan='5'><img src='images/logo.png' height='65%' width='100%'></td>
-			<!--1350x160-->
-		</tr>
+		<img src='images/logo.png' height='65%' width='100%'>
 		<!--Nav_Tabs-->
-		<tr align='center' bgcolor='lightgrey' class='td_bor'>
-			<td width='5%'> <?php Session_start();
-							if (isset($_SESSION["user_id"])) {
-								echo "<a href='user_page.php'>";
-							} else {
-								echo "<a href='home.php'>";
-							} ?>Home </a></td>
-			<td width='5%'> <a href='send_message.php'>Send Message </a></td>
-			<td width='5%'> <a href='inbox.php'>Inbox (Only Recent Message) </a></td>
-			<td width='5%'> <a href='view_profile.php'>View Profile </a></td>
-			<td width='5%'> <a href='signout.php'>Signout </a></td>
+		<table width='100%'>
+			<tr>
+				<td valign='top'>
+					<div class="vertical-menu-long">
+						<?php Session_start();
+						if (isset($_SESSION["user_id"])) {
+							echo "<a href='user_page.php'>";
+						} else {
+							echo "<a href='home.php'>";
+						} ?>Home </a>
+						<a href='send_message.php'>Send Message </a>
+						<a href='inbox.php'>Inbox (Only Recent Message) </a>
+						<a href='view_profile.php'>View Profile </a>
+						<a href='signout.php'>Signout </a>
+					</div>
+				</td>
+			</tr>
 
-		</tr>
-
-		<tr>
-			<td>
-				<hr>
-			</td>
-			<td>
-				<hr>
-			</td>
-			<td>
-				<hr>
-			</td>
-			<td>
-				<hr>
-			</td>
-			<td>
-				<hr>
-			</td>
-		</tr>
-
-		<?php
-		//Session_start();
-		if (isset($_SESSION["user_id"])) {
-			echo "<tr> <td colspan='5' align='center'> <table align='center'>
+			<?php
+			//Session_start();
+			if (isset($_SESSION["user_id"])) {
+				echo "<tr> <td colspan='5' align='center'> <table align='center'>
 							<tr  align='center'>
 								<td align='right'>Name:- </td> <td align='left'>" . $_SESSION["name"] . " </td> </tr> 
 								<tr align='center'>
@@ -60,16 +44,17 @@
 											<td align='right'>Age:- </td> <td align='left'>" . $_SESSION["age"] . "  </td> </tr>
 											<tr align='center'>
 												<td align='right'>Password:- </td> <td align='left'>" . $_SESSION["password"] . "  </td> </tr> </table> </td> </tr>";
-		} else {
-			echo "<tr align='center'> <td colspan='5'> <font color='red'> Sorry, You not Logged in! </font> Login again:- <a href='login.php'>Login</a> </td> </tr>";
-		}
+			} else {
+				echo "<tr align='center'> <td colspan='5'> <font color='red'> Sorry, You not Logged in! </font> Login again:- <a href='login.php'>Login</a> </td> </tr>";
+			}
 
-		?>
+			?>
 
-	</table>
-	<footer align='center'>
-		&copy; All rights Reserved https://github.com/abhn/simple-php-mysql-project.
-	</footer>
-</body>
+		</table>
+		<footer align='center'>
+			&copy; All rights Reserved https://github.com/abhn/simple-php-mysql-project.
+		</footer>
+	</body>
+</div>
 
 </html>
